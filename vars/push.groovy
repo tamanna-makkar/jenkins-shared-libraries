9d7738c3596d72f9aa3a6df4cee37b5c356b646e
Tamanna <tani.makkar30@gmail.com>
2026-06-07 10:03:11 +0530

    Added steps

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,11 +1,11 @@
-def call(String credId, String imageName) {
+def call(String credId, String appName, String imageName) {
     withCredentials([usernamePassword(
         credentialsId: credId,
         passwordVariable: 'dockerHubPass',
         usernameVariable: 'dockerHubUsername'
     )]) {
             sh "docker login -u ${env.dockerHubUsername} -p ${env.dockerHubPass}"
-            sh "docker image tag ${imageName} ${env.dockerHubUsername}/${imageName}:main"
+            sh "docker image tag ${appName} ${env.dockerHubUsername}/${imageName}:main"
             sh "docker push ${env.dockerHubUsername}/${imageName}:main"
         }
 }
